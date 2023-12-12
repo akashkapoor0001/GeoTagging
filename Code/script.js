2a1bd62596a9cf2770d69cea66e9719215748201
AKASH KAPOOR <akashkapoor0010@outlook.com>
2023-12-13 00:25:17 +0530

    committed

--- Code/script.js
+++ Code/script.js
@@ -5,7 +5,7 @@ const markersData = [
     { position: { lat: 22.5726, lng: 88.3639 }, title: 'Kolkata', Username: 'User4', HouseNo: '101', CameraInfo: 'Camera D' }
 ];
 
-let map;
+let map; // Declare map variable in the global scope
 
 function initMap() {
     const center = { lat: 20.5937, lng: 78.9629 };
@@ -27,55 +27,68 @@ function addMarker(map, markerData) {
         position: markerData.position,
         map: map,
         title: markerData.title,
-        animation: google.maps.Animation.DROP
+        icon: markerData.iconUrl || null
     });
 
+    let isSatelliteView = true;
 
-
-let isSatelliteView = true;
-
-document.getElementById('switchView').addEventListener('click', function() {
-    if (isSatelliteView) {
-        map.setMapTypeId('terrain');
-        this.classList.remove('satellite');
-        this.classList.add('terrain');
-        isSatelliteView = false;
-    } else {
-        map.setMapTypeId('satellite');
-        this.classList.remove('terrain');
-        this.classList.add('satellite');
-        isSatelliteView = true;
-    }
-});
-
-const infoWindow = new google.maps.InfoWindow({
-    content: `<div>
-                <strong>${markerData.title}</strong><br>
-                Username: ${markerData.Username}<br>
-                House No.: ${markerData.HouseNo}<br>
-                Camera Info: ${markerData.CameraInfo}<br>
-                <div><button style="background-color: red; color: white; margin-top: 10px;" onclick="alert('Calling now...'); window.location.href='tel:1234567890';">Call Now</button></div>
-            </div>`
-});
-
-marker.addListener('click', () => {
-    // Zoom in on the clicked marker with a slow transition
-    const targetZoom = 20;
-    const currentZoom = map.getZoom();
-    const zoomStep = (targetZoom - currentZoom) / 20; // Adjust the number of steps as needed
-
-    const zoomInterval = setInterval(() => {
-        if (map.getZoom() < targetZoom) {
-            map.setZoom(map.getZoom() + zoomStep);
+    document.getElementById('switchView').addEventListener('click', function () {
+        if (isSatelliteView) {
+            map.setMapTypeId('terrain');
+            this.classList.remove('satellite');
+            this.classList.add('terrain');
+            isSatelliteView = false;
         } else {
-            clearInterval(zoomInterval);
+            map.setMapTypeId('satellite');
+            this.classList.remove('terrain');
+            this.classList.add('satellite');
+            isSatelliteView = true;
         }
-    }, 100); // Adjust the interval as needed
+    });
 
-    // Center the map on the clicked marker
-    map.setCenter(marker.getPosition());
+    const infoWindow = new google.maps.InfoWindow({
+        content: `<div>
+                    <strong>${markerData.title}</strong><br>
+                    Username: ${markerData.Username}<br>
+                    House No.: ${markerData.HouseNo}<br>
+                    Camera Info: ${markerData.CameraInfo}<br>
+                    <div><button style="background-color: red; color: white; margin-top: 10px;" onclick="alert('Calling now...'); window.location.href='tel:1234567890';">Call Now</button></div>
+                </div>`
+    });
 
-    // Open the info window
-    infoWindow.open(map, marker);
-});
+    marker.addListener('click', () => {
+        // Zoom in on the clicked marker with a slow transition
+        const targetZoom = 20;
+        const currentZoom = map.getZoom();
+        const zoomStep = (targetZoom - currentZoom) / 15;
+
+        // Calculate the new center for smooth zoom
+
+
+        // Perform zoom steps
+        const zoomInterval = setInterval(() => {
+            if (map.getZoom() < targetZoom) {
+                map.setZoom(map.getZoom() + zoomStep);
+            } else {
+                clearInterval(zoomInterval);
+            }
+        }, 200);
+            // Center the map on the clicked marker
+            map.panTo(marker.getPosition());
+
+            // Open the info window
+            infoWindow.open(map, marker);
+        });
 }
+
+
+
+
+
+//         // Center the map on the clicked marker
+//         map.panTo(marker.getPosition());
+
+//         // Open the info window
+//         infoWindow.open(map, marker);
+//     });
+// }
